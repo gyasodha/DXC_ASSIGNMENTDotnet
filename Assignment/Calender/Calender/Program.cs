26f97797e54707cbc16684f221caf9b4b5e5d5f1
Yasodha Priya <g.yasodhapriya@dxc.com>
2023-04-21 15:27:31 +0530

    intialcommit

---
+++ Assignment/Calender/Calender/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the month (1-12): ");
+        int month = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the year: ");
+        int year = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the day of the week for the first date of the month (1-7): ");
+        int dayOfWeek = int.Parse(Console.ReadLine());
+        Console.WriteLine("Mo Tu We Th Fr Sa Su");
+        int daysInMonth = DateTime.DaysInMonth(year, month);
+        for (int i = 1; i < dayOfWeek; i++)
+        {
+            Console.Write("   ");
+        }
+        for (int day = 1; day <= daysInMonth; day++)
+        {
+            Console.Write("{0,2} ", day);
+            if ((dayOfWeek + day - 1) % 7 == 0)
+            {
+                Console.WriteLine();
+            }
+        }
+        if ((dayOfWeek + daysInMonth - 1) % 7 != 0)
+        {
+            Console.WriteLine();
+        }
+    }
+}
